74163aef3466076b73dae2ecf7ea255f68dae274
Sagar Regmi <regmi.sagar244@gmail.com>
2018-11-13 16:52:16 +0545

    Data displayed in DataGrid

---
+++ ASEB/WindowsFormsApplication4/WindowsFormsApplication4/Form2.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.dataView = new System.Windows.Forms.DataGridView();
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(13, 13);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click_1);
+            // 
+            // dataView
+            // 
+            this.dataView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataView.Location = new System.Drawing.Point(27, 57);
+            this.dataView.Name = "dataView";
+            this.dataView.Size = new System.Drawing.Size(642, 284);
+            this.dataView.TabIndex = 1;
+            // 
+            // openFile
+            // 
+            this.openFile.FileName = "openFile";
+            // 
+            // Form2
+            // 
+            this.ClientSize = new System.Drawing.Size(681, 343);
+            this.Controls.Add(this.dataView);
+            this.Controls.Add(this.btnLoad);
+            this.Name = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.dataView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.DataGridView dataView;
+        private System.Windows.Forms.OpenFileDialog openFile;
+    }
+}
---
+++ ASEB/WindowsFormsApplication4/WindowsFormsApplication4/Form2.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnLoad_Click_1(object sender, EventArgs e)
+        {
+            bool flag = this.openFile.ShowDialog() != DialogResult.Cancel;
+            if (flag)
+            {
+                string data = "";
+                int a = 0;
+                int b = 0;
+                int totalRows = 0;
+                int rows = 0;
+
+                dataView.ColumnCount = 6;
+                dataView.Columns[0].Name = "Heart Rates";
+                dataView.Columns[1].Name = "Speed";
+                dataView.Columns[2].Name = "Cadence";
+                dataView.Columns[3].Name = "Altitude";
+                dataView.Columns[4].Name = "Power";
+                dataView.Columns[5].Name = "Power Balance and Pedaling Index";
+
+                StreamReader streamReader = new StreamReader(openFile.FileName);
+                string[] filelines = File.ReadAllLines(openFile.FileName);
+                foreach (string line in filelines)
+                {
+                    if (line.Equals("[HRData]"))
+                    {
+                        a = b;
+                    }
+                    if (a > 0)
+                    {
+                        data += line + "\r\n";
+                    }
+                    b++;
+
+                    dataView.AllowUserToAddRows = false;
+                    string text = "";
+                    for (text = streamReader.ReadLine(); text != null; text = streamReader.ReadLine())
+                    {
+                       
+                        
+
+                            string[] array = text.Split('\t');
+                            dataView.Rows.Add(array);
+                        }
+                        //fileReading.Text = b + data;
+
+                        //int s = totalRows - rows;
+                        //string[] arrayData = new string[s];
+
+                        //string heart = "";
+                        //string speed = "";
+                        //string cadence = "";
+                        //string altitude = "";
+                        //string power = "";
+                        //string pbpi = "";
+                        //if (rows != 0)
+                        //{
+                        //    foreach (string line in filelines)
+                        //    {
+                        //        if (rows < b)
+                        //        {
+                        //            data += line + "\r\n";
+                        //            string[] words = line.Split('\t');
+                        //            for (int x = 0; x < words.Length; x++)
+                        //            {
+                        //                if (x == 0) heart = words[x];
+                        //                else if (x == 1) speed = words[x];
+                        //                else if (x == 2) cadence = words[x];
+                        //                else if (x == 3) altitude = words[x];
+                        //                else if (x == 4) power = words[x];
+                        //                else pbpi = words[x];
+                        //            }
+                        //            string[] row_val = new string[] { heart, speed, cadence, altitude, power, pbpi };
+                        //            dataView.Rows.Add(row_val);
+                        //        }
+                        //        else b++;
+                        //    }
+                        //}
+
+                        // fileReading.Text = data;
+                    }
+                }
+            }
+        }
+    }
+
+
+
+
